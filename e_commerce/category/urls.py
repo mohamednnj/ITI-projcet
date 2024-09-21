7be9248e7cc144a6656122eb7637df0b0bbb9a4c
Mohamed Ashraf <144217537+mhmd-ashrf-saad@users.noreply.github.com>
2024-09-21 15:21:48 +0300

    Category

--- e_commerce/category/urls.py
+++ e_commerce/category/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-
+from .views import*
 urlpatterns = [
-    
+    path('', category,name="category")
 ]
 
 urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
