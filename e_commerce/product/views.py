f663142293ed7175adcc32c717e03e02d425199b
Mohamed Ashraf <144217537+mhmd-ashrf-saad@users.noreply.github.com>
2024-09-22 15:16:59 +0300

    Products
    products page without filter

--- e_commerce/product/views.py
+++ e_commerce/product/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def product(req):
+    products = Product.objects.all()
+    return render(req,'shop/index.html',{'products':products} )
