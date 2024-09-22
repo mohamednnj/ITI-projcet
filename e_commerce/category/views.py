692c83aa065a775d323aaf8d6f33b6a928778791
mohamednnj <110845833+mohamednnj@users.noreply.github.com>
2024-09-22 21:37:27 +0300

    set image setting

--- e_commerce/category/views.py
+++ e_commerce/category/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Category
 # Create your views here.
 def category(req):
-    return render(req,'category/index.html', )
+    categorys = Category.objects.all()
+    return render(req,'category/index.html', {'categorys':categorys})
