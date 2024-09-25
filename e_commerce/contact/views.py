d494032d238944606c9b52151ec1820cb9787083
Mohamed Ashraf <144217537+mhmd-ashrf-saad@users.noreply.github.com>
2024-09-25 14:59:55 +0300

    contact page & success page
    html,css,settings,views,forms,urls
    all done

---
+++ e_commerce/contact/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,redirect
+from accounts.forms import  loginFrom
+from contact.forms import ContactForm
+from django.http import HttpResponse
+# Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            return redirect('success')
+    else:
+        form = ContactForm()
+    return render(request, 'contact/index.html', {'form': form})
+   
+
+def success(request):
+    return render(request, 'contact/success.html')
