2df46ecf8de2a9a19c6ddb82bc6ef49d5b6b0204
Mohamed Ashraf <144217537+mhmd-ashrf-saad@users.noreply.github.com>
2024-09-25 15:45:18 +0300

    added classes to forms

--- e_commerce/contact/forms.py
+++ e_commerce/contact/forms.py
@@ -1,10 +1,43 @@
 from django import forms
 from django.core.validators import EmailValidator
 
-
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.CharField(validators=[EmailValidator()])
-    phone = forms.CharField(max_length=15)
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'name',
+            'class': 'contact-form-control',
+        })
+    )
+    
+    email = forms.CharField(
+        validators=[EmailValidator()],
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'email',
+            'class': 'contact-form-control',
+        })
+    )
+    
+    phone = forms.CharField(
+        max_length=15,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'phone',
+            'class': 'contact-form-control',
+        })
+    )
+    
+    subject = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'subject',
+            'class': 'contact-form-control',
+        })
+    )
+    
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'placeholder': 'message',
+            'class': 'contact-form-control',
+            'rows': 3,
+        })
+    )
