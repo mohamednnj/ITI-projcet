f7eeefdc9856290dcec5cad7487137a0623aff35
Mohamed Ashraf <144217537+mhmd-ashrf-saad@users.noreply.github.com>
2024-09-27 18:52:21 +0300

    cart

--- e_commerce/cart/models.py
+++ e_commerce/cart/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from product.models import Product
-from accounts.models import User,Product
-# Create your models here.
+from accounts.models import User
+
 class Cart(models.Model):
-    customr = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
+    customer = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
     selected_product = models.ManyToManyField(Product)
+    
+    def __str__(self):
+        return f"Cart of {self.customer}"
--- e_commerce/product/views.py
+++ e_commerce/product/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from category.models import Category
 from .models import Product
+
 from accounts.forms import SignupForm, loginForm
+
 # Create your views here.
 def product(req):
     categorys = Category.objects.all()
@@ -23,3 +25,4 @@ def product_by_category(request, category_id):
     category = get_object_or_404(Category, id=category_id)  # Get the category by id
     products = Product.objects.filter(category=category)  # Get all products related to the category
     return render(request, 'shop/product_by_category.html', {'category': category, 'products': products})    
+
