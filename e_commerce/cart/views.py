3939c374ee8e7741e6981b2f6d0b90df41736aaa
mohamednnj <110845833+mohamednnj@users.noreply.github.com>
2024-09-28 05:18:47 +0300

    delete cart app

--- e_commerce/cart/views.py
+++ e_commerce/cart/views.py
@@ -1,36 +1,3 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
-from product.models import *
-from .models import *
-import json
+from django.shortcuts import render
 
-def add_to_cart(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        product_id = data.get('product_id')
-        quantity = data.get('quantity')
-
-        # Logic to add the product to the cart
-        # e.g., Cart.objects.create(product=product, quantity=quantity)
-
-        return JsonResponse({'success': True})
-
-    return JsonResponse({'success': False}, status=400)
-
-def get_cart(request):
-    if request.user.is_authenticated:
-        cart = Cart.objects.filter(customer=request.user).first()
-        if cart:
-            products = cart.selected_product.all()
-            product_data = [
-                {
-                    'name': product.name,
-                    'price': product.price,
-                    'image': product.image.url,
-                }
-                for product in products
-            ]
-            return JsonResponse({"products": product_data})
-    
-    return JsonResponse({"error": "Cart not found"}, status=404)
+# Create your views here.
